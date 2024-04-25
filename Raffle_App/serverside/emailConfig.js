05036d03b53f0b7a323284b1d8d8e8fb9bd99492
Antonia1365 <56393240+Antonia1365@users.noreply.github.com>
2024-04-25 09:19:37 +0100

    Github restrictions expired notification, fix and make public

--- Raffle_App/serverside/emailConfig.js
+++ Raffle_App/serverside/emailConfig.js
@@ -1,14 +1,14 @@
 // config
-// Ideally should be set as evn variables
-// like set MAIL_USERNAME = "sally.mn1245@gmail.com"
-// but to ensure it runs on different setups:
+// Ideally should be set as evn variables, but had issues accessing them on some devices
+// set command: set MAIL_USERNAME = "exampleEmail@gmail.com"
+// but to ensure it runs on different setups, put them in here:
 
-MAIL_USERNAME = "sally.mn1245@gmail.com";
-MAIL_PASSWORD = "SailingSally45"
+MAIL_USERNAME = "exampleEmail@gmail.com";
+MAIL_PASSWORD = "examplePassword"
 MAIL_RECEPIENT = "";
-OAUTH_CLIENTID = "457446171786-d55acq0r4uojssbem6fkrahm92fmd53o.apps.googleusercontent.com";
-OAUTH_CLIENT_SECRET = "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";
-OAUTH_REFRESH_TOKEN = "1//04vjonRICrQ_3CgYIARAAGAQSNwF-L9Iriv4i_TuGSaFfGqYZUBBOGFNm0PG2x5QF1qEgo1D-WjXUyV9fx_gHaAJMhHufnQZvW2I";
+OAUTH_CLIENTID = "xxx";
+OAUTH_CLIENT_SECRET = "xxx";
+OAUTH_REFRESH_TOKEN = "xxx";
 
 module.exports = { MAIL_USERNAME, MAIL_PASSWORD,OAUTH_CLIENTID, OAUTH_CLIENT_SECRET, OAUTH_REFRESH_TOKEN};
 
